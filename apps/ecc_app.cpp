77f3ee2475d7477c446f20dc18edb1583a998cbe
pichristoph <pichristoph@hotmail.com>
2021-11-04 22:43:24 +0100

    add option to correct circuit regularly; started NinjaStar (Q9Surface)

--- apps/ecc_app.cpp
+++ apps/ecc_app.cpp
@@ -9,6 +9,8 @@
 #include <eccs/Ecc.hpp>
 #include <eccs/IdEcc.hpp>
 #include <eccs/Q3ShorEcc.hpp>
+#include <eccs/Q7SteaneEcc.hpp>
+#include <eccs/Q9SurfaceEcc.hpp>
 #include <eccs/Q9ShorEcc.hpp>
 
 #include "ecc/EccMapper.hpp"
@@ -22,6 +24,7 @@ int main(int argc, char** argv) {
             ("in", po::value<std::string>()->required(), "File to read from")
             ("out", po::value<std::string>()->required(), "File to write to")
             ("ecc", po::value<std::string>()->required(), "Error correcting code to use")
+            ("freq", po::value<int>(), "frequency of error correction (0=at the end, n>0 = after every n-th gate)")
             ("ps", "print statistics")
             ("verbose", "Increase verbosity and output additional information to stderr")
             ;
@@ -38,6 +41,11 @@ int main(int argc, char** argv) {
         std::exit(1);
     }
 
+    int measureFrequency = 0;
+    if(vm.count("freq")) {
+        measureFrequency = vm["freq"].as<int>();
+    }
+
 
 	const std::string circuit = vm["in"].as<std::string>();
 	qc::QuantumComputation qc{};
@@ -50,23 +58,29 @@ int main(int argc, char** argv) {
 		std::exit(1);
 	}
 
-	qc::MatrixDD test;
+	//qc::MatrixDD test;
     Ecc *mapper = nullptr;
 
     const std::string eccName = vm["ecc"].as<std::string>();
 
     if(eccName.compare(IdEcc::getName())==0) {
-        mapper = new IdEcc(qc);
+        mapper = new IdEcc(qc, measureFrequency);
     } else if(eccName.compare(Q3ShorEcc::getName())==0) {
-        mapper = new Q3ShorEcc(qc);
+        mapper = new Q3ShorEcc(qc, measureFrequency);
+    } else if(eccName.compare(Q7SteaneEcc::getName())==0) {
+        mapper = new Q7SteaneEcc(qc, measureFrequency);
     } else if(eccName.compare(Q9ShorEcc::getName())==0) {
-        mapper = new Q9ShorEcc(qc);
+        mapper = new Q9ShorEcc(qc, measureFrequency);
+    } else if(eccName.compare(Q9SurfaceEcc::getName())==0) {
+        mapper = new Q9SurfaceEcc(qc, measureFrequency);
     } else {
         std::cerr << "No ECC found for " << eccName << std::endl;
         std::cerr << "Available ECCs: ";
         std::cerr << IdEcc::getName() << ", ";
         std::cerr << Q3ShorEcc::getName() << ", ";
-        std::cerr << Q9ShorEcc::getName() << std::endl;
+        std::cerr << Q7SteaneEcc::getName() << ", ";
+        std::cerr << Q9ShorEcc::getName() << ", ";
+        std::cerr << Q9SurfaceEcc::getName() << std::endl;
         std::exit(1);
     }
 
